6c3d9cf6ae9c9187aa2d236fbeb9be2e51332c16
Bradley <bradleyjesseb@gmail.com>
2023-02-24 17:21:01 -0400

    Max Balance, Pause/Unpause

--- contracts/PeliWallet.sol
+++ contracts/PeliWallet.sol
@@ -2,19 +2,36 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract PeliWallet is Ownable{
+contract PeliWallet is Ownable, Pausable {
+    using SafeMath for uint256;
 
+    constructor() {}
 
-    constructor(){}
+    receive() external payable {}
 
-    receive() external payable{} //special function
-
-    function withdraw(uint _amount) external onlyOwner{
+    function withdraw(uint _amount) external onlyOwner whenNotPaused {
+        require(address(this).balance >= _amount, "Insufficient balance in wallet");
         payable(msg.sender).transfer(_amount);
     }
 
-    function getBalance() external view returns (uint){
+    function deposit() external payable whenNotPaused {
+        // use SafeMath to prevent overflow
+        uint256 newBalance = address(this).balance.add(msg.value);
+        require(newBalance <= 10 ether, "Wallet balance limit reached");
+    }
+
+    function getBalance() external view returns (uint256) {
         return address(this).balance;
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
     }
 }
