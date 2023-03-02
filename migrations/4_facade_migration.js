cd0e33db98d83247e73bb88f4cc5ee8d98505cce
Bradley <bradleyjesseb@gmail.com>
2023-03-02 09:53:01 -0400

    .

--- migrations/4_facade_migration.js
+++ migrations/4_facade_migration.js
@@ -1,5 +1,5 @@
 var PeliWalletFacade = artifacts.require("PeliWalletFacade");
 
 module.exports = function(deployer) {
-  deployer.deploy(PeliWalletFacade, "0x79D3A96646f2ee21bA07AA9fC969c7853599ebD9");
+  deployer.deploy(PeliWalletFacade, "0x493B20eD28A3F59Ee11cf80B7cF98b7e7D92fcB1");
 };
